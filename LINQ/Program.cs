db55365504c8c8da16fc7d18377457765d954580
Nolan Martin <nmartinsdtc@gmail.com>
2023-03-27 13:03:07 -0600

    created list of video games and sorted the list by length of name

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace LINQ
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videoGames = new List<string>()
+            {
+                "Call of Duty: Modern Warfare",
+                "Call of Duty: Black Ops",
+                "Overwatch",
+                "Fortnite",
+                "Diablo",
+                "GTA"
+            };
+
+            Console.WriteLine("Shortest to Longest Video Game Names:\n");
+            videoGames.OrderBy(game => game.Length).ToList().ForEach(Console.WriteLine);
+
         }
     }
 }
